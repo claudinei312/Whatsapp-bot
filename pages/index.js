80c3e8ff518e090af562a22e804d72e717f2e35f
claudinei312 <129322307+claudinei312@users.noreply.github.com>
2025-05-15 08:17:59 -0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>WhatsApp Bot API</h1>
-      <p>Use the /api/enviar endpoint to start WhatsApp client and generate QR code on the server console.</p>
-    </div>
-  );
-}
